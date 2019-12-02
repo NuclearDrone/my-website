a4462b6e74e55ed308ad24dfdf1652b1f81bcfb3
NuclearDrone <enbergmicke99@gmail.com>
2019-12-02 11:14:19 +0200

    Add logo to header.

--- frontend/src/components/shared/Header/index.js
+++ frontend/src/components/shared/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+const logo = require('../../../images/NDlogo.svg')
 
 const Header = () => {
   return (
     <div className='header'>
+      <img src={logo} alt='logo' className='logo' />
       <p className='header-item'>TEST HEADER</p>
       <p className='header-item'>TEST HEADER</p>
       <p className='header-item'>TEST HEADER</p>
