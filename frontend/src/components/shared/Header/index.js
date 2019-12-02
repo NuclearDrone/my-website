fcf2a7d32a06eb92a750c8bf76e9286369dbd62b
Micke Enberg <micke.enberg@aalto.fi>
2019-12-02 13:20:20 +0200

    Refactored css to mobile first, add responsive header.

--- frontend/src/components/shared/Header/index.js
+++ frontend/src/components/shared/Header/index.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 const logo = require('../../../images/NDlogo.svg')
 
 const Header = () => {
+  const [showMobileHeader, toggleMobileHeader] = useState(false)
+
+  const toggle = () => toggleMobileHeader(!showMobileHeader)
   return (
     <div className='header'>
-      <img src={logo} alt='logo' className='logo' />
-      <p className='header-item'>TEST HEADER</p>
-      <p className='header-item'>TEST HEADER</p>
-      <p className='header-item'>TEST HEADER</p>
-      <p className='header-item'>TEST HEADER</p>
+      <div className='headerLogo'>
+        <img src={logo} alt='logo' className='logo' />
+      </div>
+      <div className='header-items-desktop'>
+        <p className='header-item'>TEST HEADER</p>
+        <p className='header-item'>TEST HEADER</p>
+        <p className='header-item'>TEST HEADER</p>
+        <p className='header-item'>TEST HEADER</p>
+      </div>
+      <div className='header-mobile'>
+        <p onClick={toggle}>Open</p>
+        {showMobileHeader && (
+          <div className='header-items-mobile'>
+            <p className='header-item'>TEST HEADER</p>
+            <p className='header-item'>TEST HEADER</p>
+            <p className='header-item'>TEST HEADER</p>
+            <p className='header-item'>TEST HEADER</p>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
